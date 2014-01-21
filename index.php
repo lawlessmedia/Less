6561af43dfb2874cf56021faa90e8fa7e4805d7c
David Lawless <david@chartspan.com>
2014-01-21 00:01:21 -0500

    New Phoresia stylie | added fixed transparent head and numerous styling tweaks

--- index.php
+++ index.php
@@ -24,16 +24,16 @@
 <header id="masthead" class="site-header" role="banner">
 	<div class="container">
 		
-		<div class="gravatar">
+		<!--<div class="gravatar">
 			<?php 
 				// grab admin email and their photo
 				$admin_email = get_option('admin_email');
 				echo get_avatar( $admin_email, 100 ); 
 			?>
-		</div><!--/ author -->
+		</div>--><!--/ author -->
 		
 		<div id="brand">
-			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a> &mdash; <span><?php echo get_bloginfo( 'description' ); ?></span></h1>
+			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a><span><?php echo get_bloginfo( 'description' ); ?></span></h1>
 		</div><!-- /brand -->
 	
 		<nav role="navigation" class="site-navigation main-navigation">
@@ -80,7 +80,7 @@
 						</div><!--/post-meta -->
 						
 						<div class="the-content">
-							<?php the_content( 'Continue...' ); ?>
+							<?php the_excerpt( 'Continue...' ); ?>
 							
 							<?php wp_link_pages(); ?>
 						</div><!-- the-content -->
